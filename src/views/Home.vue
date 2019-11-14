307d82ad58370885f1dbb08a8d5a3d7d0a17c354
张一凡 <1036898516@qq.com>
2019-11-14 18:23:23 +0800

    配置view-design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <iButton>按钮</iButton>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {Button} from 'view-design'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    iButton:Button
   }
 }
 </script>
